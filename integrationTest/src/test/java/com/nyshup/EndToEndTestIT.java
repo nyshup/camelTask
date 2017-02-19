bf75cb663d97b29e97827c491568bc4e4c16a748
Ruslan Pushyn <nyshup@gmail.com>
2017-02-19 23:56:30 +0200

    Fix test. Remove maven warnings

--- integrationTest/src/test/java/com/nyshup/EndToEndTestIT.java
+++ integrationTest/src/test/java/com/nyshup/EndToEndTestIT.java
@@ -13,12 +13,22 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import javax.jms.Destination;
 import javax.jms.TextMessage;
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = TestConfig.class)
 public class EndToEndTestIT {
 
-    @Value("${file.dir}")
+    public static final String TEST_BODY = "TEST BODY";
+    @Value("${output.file.dir}")
     private String testFileDir;
 
 
@@ -27,17 +37,29 @@ public class EndToEndTestIT {
 
     @Test(timeout = 1000000)
     public void testMessagePost() throws Exception {
-        File directory = new File(testFileDir);
-        FileUtils.deleteDirectory(directory);
+        deleteOutputDirectory();
         Destination destination = ActiveMQDestination.createDestination("serviceQueue", ActiveMQDestination.QUEUE_TYPE);
         jmsTemplate.send(destination, session -> {
             TextMessage message = session.createTextMessage();
-            message.setText("TEST BODY");
+            message.setText(TEST_BODY);
             message.setStringProperty("MyAction", "TEST Action");
             return message;
         });
         Thread.sleep(2000);
-        File[] listOfFiles = directory.listFiles();
+        Path path = Paths.get(testFileDir);
+        assertTrue("Directory should be created", Files.exists(path));
+        List<Path> files = Files.list(path).collect(Collectors.toList());
+        assertEquals("One file should be created", 1, files.size());
+        assertEquals("TEST BODY", fileToString(files.get(0)));
+    }
+
+    private void deleteOutputDirectory() throws IOException {
+        File directory = new File(testFileDir);
+        FileUtils.deleteDirectory(directory);
+    }
+
+    private String fileToString(Path file) throws IOException {
+        return new String(Files.readAllBytes(file));
     }
 
 }
